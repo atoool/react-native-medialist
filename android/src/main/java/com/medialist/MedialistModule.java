c6fa7e204a23f0551f057f76b001dc968f36271d
atoool <athulchelad@gmail.com>
2020-06-27 11:59:36 +0530

    updated java code

--- android/src/main/java/com/medialist/MedialistModule.java
+++ android/src/main/java/com/medialist/MedialistModule.java
@@ -1,10 +1,8 @@
 package com.medialist;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import wseemann.media.FFmpegMediaMetadataRetriever;
-import android.util.Base64;
-import android.graphics.Matrix;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.MediaStore;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -13,9 +11,9 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableMap;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
 
 public class MedialistModule extends ReactContextBaseJavaModule {
 
@@ -31,112 +29,147 @@ public class MedialistModule extends ReactContextBaseJavaModule {
         return "Medialist";
     }
 
-    private final String[] metadatas = { "album", "album_artist", "comment", "copyright", "creation_time", "disc",
-            "encoder", "encoded_by", "genre", "language", "performer", "publisher", "service_name", "service_provider",
-            "track", "variant_bitrate", "icy_metadata", "framerate", "chapter_start_time", "chapter_end_time", "artist",
-            "composer", "title", "date", "duration", "rotation" };
-
-    private String convertToBase64(byte[] bytes) {
-        return Base64.encodeToString(bytes, Base64.NO_WRAP);
-    }
-
-    private byte[] convertToBytes(Bitmap bmp) {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.PNG, 75, stream);
-        return stream.toByteArray();
-    }
-
-    private void putString(WritableMap map, String key, String value) {
-        if (value != null)
-            map.putString(key, value);
-    }
-
-    private void getMetadata(String path, ReadableMap options, Promise promise) {
-        File f = new File(path);
-        if (!f.exists() || f.isDirectory()) {
-            promise.reject("-15", "file not found");
+    @ReactMethod
+    public void getAssets(final ReadableMap params, final Promise promise) {
+        if (isJellyBeanOrLater()) {
+            promise.reject(new Exception("Version of Android must be > JellyBean"));
             return;
         }
 
-        FFmpegMediaMetadataRetriever mmr = new FFmpegMediaMetadataRetriever();
-        WritableMap result = Arguments.createMap();
+        String requestedType = "all";
+        if (params.hasKey("type")) {
+            requestedType = params.getString("type");
+        }
+
+        Integer limit = 10;
+        if (params.hasKey("limit")) {
+            limit = params.getInt("limit");
+        }
+        Integer startFrom = 0;
+        if (params.hasKey("startFrom")) {
+            startFrom = params.getInt("startFrom");
+        }
+        String albumName = null;
+        if (params.hasKey("albumName")) {
+            albumName = params.getString("albumName");
+        }
+
+        WritableMap response = new WritableNativeMap();
+
+        Cursor gallery = null;
         try {
-            mmr.setDataSource(path);
+            gallery = GalleryCursorManager.getAssetCursor(requestedType, albumName, reactContext);
+            WritableArray assets = new WritableNativeArray();
 
-            // check is media
-            String audioCodec = mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_AUDIO_CODEC);
-            String videoCodec = mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_CODEC);
-
-            if (audioCodec == null && videoCodec == null) {
-                promise.resolve(result);
-                mmr.release();
+            if (gallery.getCount() <= startFrom) {
+                promise.reject("gallery index out of bound", "");
                 return;
-            }
-
-            // get all metadata - TODO :: Loop through only values for audio or video
-            // metadata based on media type for a small performance boost
-            for (String meta : metadatas) {
-                putString(result, meta, mmr.extractMetadata(meta));
-            }
-
-            if (result.hasKey("framerate") && !result.hasKey("rotation")) {
-                putString(result, "rotation",
-                        mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION));
-            }
-
-            // Legacy support & camelCase
-            result.putString("createTime", result.getString("creation_time"));
-
-            if (options.getBoolean("getThumb")) {
-                // get thumb
-                Bitmap bmp = mmr.getFrameAtTime();
-                if (bmp != null) {
-                    // Bitmap bmp2 = mmr.getFrameAtTime((long) 4E6,
-                    // FFmpegMediaMetadataRetriever.OPTION_CLOSEST);
-                    // if (bmp2 != null) bmp = bmp2;
-
-                    /*
-                     * The image returned seems to be always in landscape mode and does not follow
-                     * the rotation of the video. get the rotation from the metadata and apply the
-                     * correction so the image is straight.
-                     */
-
-                    if (result.hasKey("rotation")) {
-                        Bitmap rotatedBmp = RotateBitmap(bmp, Float.parseFloat(result.getString("rotation")));
-                        if (rotatedBmp != null) {
-                            bmp = rotatedBmp;
-                        }
-                    }
-
-                    byte[] bytes = convertToBytes(bmp);
-                    result.putInt("width", bmp.getWidth());
-                    result.putInt("height", bmp.getHeight());
-                    result.putString("thumb", convertToBase64(bytes));
+            } else {
+                response.putInt("totalAssets", gallery.getCount());
+                boolean hasMore = gallery.getCount() > startFrom + limit;
+                response.putBoolean("hasMore", hasMore);
+                if (hasMore) {
+                    response.putInt("next", startFrom + limit);
+                } else {
+                    response.putInt("next", gallery.getCount());
                 }
+                gallery.moveToPosition(startFrom);
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            do {
+                WritableMap asset = getAsset(gallery);
+                assets.pushMap(asset);
+                if (gallery.getPosition() == (startFrom + limit) - 1)
+                    break;
+            } while (gallery.moveToNext());
+
+            response.putArray("assets", assets);
+
+            promise.resolve(response);
+
+        } catch (SecurityException ex) {
+            System.err.println(ex);
         } finally {
-            promise.resolve(result);
-            mmr.release();
+            if (gallery != null)
+                gallery.close();
         }
     }
 
-    private Bitmap RotateBitmap(Bitmap source, float angle) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(angle);
-        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    @ReactMethod
+    public void getAlbums(final Promise promise) {
+        if (isJellyBeanOrLater()) {
+            promise.reject(new Exception("Version of Android must be > JellyBean"));
+            return;
+        }
+
+        WritableMap response = new WritableNativeMap();
+
+        Cursor gallery = null;
+        try {
+            gallery = GalleryCursorManager.getAlbumCursor(reactContext);
+            WritableArray albums = new WritableNativeArray();
+            response.putInt("totalAlbums", gallery.getCount());
+            gallery.moveToFirst();
+            do {
+                WritableMap album = getAlbum(gallery);
+                albums.pushMap(album);
+            } while (gallery.moveToNext());
+
+            response.putArray("albums", albums);
+
+            promise.resolve(response);
+
+        } catch (SecurityException ex) {
+            System.err.println(ex);
+        } finally {
+            if (gallery != null)
+                gallery.close();
+        }
+
     }
 
-    @ReactMethod
-    public void get(final String path, final ReadableMap options, final Promise promise) {
+    private WritableMap getAsset(Cursor gallery) {
+        WritableMap asset = new WritableNativeMap();
+        int mediaType = gallery.getInt(gallery.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE));
+        String mimeType = gallery.getString(gallery.getColumnIndex(MediaStore.Files.FileColumns.MIME_TYPE));
+        String creationDate = gallery.getString(gallery.getColumnIndex(MediaStore.Files.FileColumns.DATE_ADDED));
+        String fileName = gallery.getString(gallery.getColumnIndex(MediaStore.Files.FileColumns.DISPLAY_NAME));
+        Double height = gallery.getDouble(gallery.getColumnIndex(MediaStore.Files.FileColumns.HEIGHT));
+        Double width = gallery.getDouble(gallery.getColumnIndex(MediaStore.Files.FileColumns.WIDTH));
+        String uri = "file://" + gallery.getString(gallery.getColumnIndex(MediaStore.Files.FileColumns.DATA));
+        Double id = gallery.getDouble(gallery.getColumnIndex(MediaStore.Files.FileColumns._ID));
 
-        new Thread() {
-            @Override
-            public void run() {
-                getMetadata(path, options, promise);
-            }
-        }.start();
+        asset.putString("mimeType", mimeType);
+        asset.putString("creationDate", creationDate);
+        asset.putDouble("height", height);
+        asset.putDouble("width", width);
+        asset.putString("filename", fileName);
+        asset.putDouble("id", id);
+        asset.putString("uri", uri);
+
+        if (mediaType == MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE) {
+            asset.putDouble("duration", 0);
+            asset.putString("type", "image");
+
+        } else if (mediaType == MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO) {
+            Double duration = gallery.getDouble(gallery.getColumnIndex(MediaStore.Video.VideoColumns.DURATION));
+            asset.putDouble("duration", duration / 1000);
+            asset.putString("type", "video");
+        }
+        return asset;
+    }
+
+    private WritableMap getAlbum(Cursor gallery) {
+        WritableMap album = new WritableNativeMap();
+        String albumName = gallery
+                .getString(gallery.getColumnIndex(MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME));
+        int assetCount = gallery.getInt(gallery.getColumnIndex("assetCount"));
+        album.putString("title", albumName);
+        album.putInt("assetCount", assetCount);
+        return album;
+    }
+
+    private Boolean isJellyBeanOrLater() {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN;
     }
 }
